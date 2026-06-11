487c676caa6fdedefdcac2e9934cd64fabd02657
alsqja03 <144881118+alsqja03@users.noreply.github.com>
2026-06-11 14:32:50 +0900

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -1,10 +1,8 @@
-// 네비게이션바 열고 닫기 함수
 function toggleNav() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
 
-// 화면 아무 곳이나 클릭하면 메뉴 닫기 (선택 사항)
 document.addEventListener('click', (e) => {
     const nav = document.querySelector('nav');
     const toggle = document.getElementById('menu-toggle');
